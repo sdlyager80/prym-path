7b7e629c49bb4411273f9d1723eb0f38fdba9fd9
sdlyager80 <sdlyager@gmail.com>
2025-09-09 19:31:25 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
-import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import './App.css'
+import Pages from "@/pages/index.jsx"
+import { Toaster } from "@/components/ui/toaster"
+import { AuthProvider } from "@/contexts/AuthContext"
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Pages />
+        <Toaster />
+      </AuthProvider>
+    </BrowserRouter>
+  )
+}
+
+export default App
